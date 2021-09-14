70fb2e2ce62cc3944ca862de656bb177336a8109
MochamadRicoPratamaPutra <mricopratamaputra@gmail.com>
2021-09-14 14:28:52 +0700

    fixing nodemailer

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -9,10 +9,10 @@ function main(name, email, id) {
   });
   transporter
     .sendMail({
-      from: '"Circle"<ricoxxx007@gmail.com>',
+      from: '"SkyTalk"<ricoxxx007@gmail.com>',
       to: `${email}`,
-      subject: 'Thank you for registering to Circle!',
-      html: `<h style:'margin-left:auto; margin-right:auto'>Thank you ${name} for registering to Circle</h><p style:'margin-left:auto; margin-right:auto'>One more step to awesome riding experience!</p><button style:'margin-left:auto; margin-right:auto;outline:none;border-radius:3px; background: red;padding: 5px 10px'><a href='${process.env.TARGET_URL}/verification/${id}'>Click here to confirm your account!</a></button>`,
+      subject: 'Thank you for registering to SkyTalk!',
+      html: `<h style:'margin-left:auto; margin-right:auto'>Thank you ${name} for registering to SkyTalk</h><p style:'margin-left:auto; margin-right:auto'>One more step to awesome riding experience!</p><button style:'margin-left:auto; margin-right:auto;outline:none;border-radius:3px; background: red;padding: 5px 10px'><a href='${process.env.TARGET_URL}/verification/${id}'>Click here to confirm your account!</a></button>`,
     })
     .then((res) => {
       // console.log(res);
