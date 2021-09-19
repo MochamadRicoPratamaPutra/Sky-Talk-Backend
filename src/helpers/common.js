cc194e59a0ac95fc63c35f9cb28f0197586956c3
MochamadRicoPratamaPutra <mricopratamaputra@gmail.com>
2021-09-19 14:22:48 +0700

    debugging nodemailer

--- src/helpers/common.js
+++ src/helpers/common.js
@@ -15,7 +15,7 @@ function main(name, email, id) {
       html: `<h style:'margin-left:auto; margin-right:auto'>Thank you ${name} for registering to SkyTalk</h><p style:'margin-left:auto; margin-right:auto'>One more step to awesome riding experience!</p><button style:'margin-left:auto; margin-right:auto;outline:none;border-radius:3px; background: red;padding: 5px 10px'><a href='${process.env.TARGET_URL}/verification/${id}'>Click here to confirm your account!</a></button>`,
     })
     .then((res) => {
-      // console.log(res);
+      console.log(res);
     })
     .catch((err) => {
       console.log('error', err);
